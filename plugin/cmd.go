956f1a0edb10f43d79ca6b292bad5c74fff58a2e
hidez8891 <minowith+github@gmail.com>
2017-07-01 14:10:00 +0900

    add idea

---
+++ plugin/cmd.go
@@ -0,0 +1,31 @@
+package plugin
+
+/*
+  // cmd
+	type Cmd struct {
+	}
+
+	func NewCmd(path string) (*Cmd, error) {
+	}
+
+	func NewCmd2(r io.Reader, w io.Writer) (*Cmd, error) {
+	}
+
+	func (o *Cmd) SendID(id int) error {
+	}
+
+	func (o *Cmd) RecvID() (int, error) {
+	}
+
+	func (o *Cmd) SendArgs(args []byte) error {
+	}
+
+	func (o *Cmd) RecvArgs() ([]byte, error) {
+	}
+
+	func (o *Cmd) SendReturn(args []byte) error {
+	}
+
+	func (o *Cmd) RecvReturn() ([]byte, error) {
+	}
+*/
